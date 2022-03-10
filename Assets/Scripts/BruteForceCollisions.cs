e86f9e92411644a654338a6bd28b1abb1a1470fa
mattcmerritt <mattcmerritt@gmail.com>
2022-03-09 21:52:49 -0500

    merge in brute-force

--- Assets/Scripts/BruteForceCollisions.cs
+++ Assets/Scripts/BruteForceCollisions.cs
@@ -15,6 +15,9 @@ public class BruteForceCollisions : MonoBehaviour
     [Range(0, 1000), SerializeField]
     private float Speed;
 
+    [SerializeField]
+    private bool UsingUnity;
+
 
     private void Start()
     {
@@ -31,45 +34,56 @@ public class BruteForceCollisions : MonoBehaviour
 
     private void Update()
     {
-        for (int c1 = 0; c1 < Circles.Count - 1; c1++)
+        if (!UsingUnity)
         {
-            for (int c2 = c1 + 1; c2 < Circles.Count; c2++)
+            for (int c1 = 0; c1 < Circles.Count - 1; c1++)
             {
-                if (Circles[c1].IsColliding(Circles[c2]))
+                for (int c2 = c1 + 1; c2 < Circles.Count; c2++)
                 {
-                    Vector3 dir = Circles[c2].transform.position - Circles[c1].transform.position;
-                    if (Vector3.Angle(dir, Circles[c1].GetComponent<Rigidbody2D>().velocity) > 180) {
-                        Debug.Log("SKIPPED");
-                        continue;
-                    }
+                    if (Circles[c1].IsColliding(Circles[c2]))
+                    {
+                        Vector3 dir = Circles[c2].transform.position - Circles[c1].transform.position;
+                        if (Vector3.Angle(dir, Circles[c1].GetComponent<Rigidbody2D>().velocity) > 180)
+                        {
+                            Debug.Log("SKIPPED");
+                            continue;
+                        }
 
-                    Debug.Log("HERE");
-                    //elastic collision 
-                    float invMassSum = Circles[c1].GetMass() + Circles[c2].GetMass();
-                    invMassSum = 1 / invMassSum;
+                        Debug.Log("HERE");
+                        //elastic collision 
+                        float invMassSum = Circles[c1].GetMass() + Circles[c2].GetMass();
+                        invMassSum = 1 / invMassSum;
 
-                    float m1Dif = Circles[c1].GetMass() - Circles[c2].GetMass();
-                    float m2Dif = -m1Dif;
+                        float m1Dif = Circles[c1].GetMass() - Circles[c2].GetMass();
+                        float m2Dif = -m1Dif;
 
-                    Vector2 vel1 = Circles[c1].GetComponent<Rigidbody2D>().velocity;
-                    Vector2 vel2 = Circles[c2].GetComponent<Rigidbody2D>().velocity;
+                        Vector2 vel1 = Circles[c1].GetComponent<Rigidbody2D>().velocity;
+                        Vector2 vel2 = Circles[c2].GetComponent<Rigidbody2D>().velocity;
 
-                    Vector2 newVel1 = (vel1 * m1Dif * invMassSum) + (vel2 * 2 * Circles[c2].GetMass() * invMassSum);
-                    Vector2 newVel2 = (vel1 * 2 * Circles[c1].GetMass() * invMassSum) + (vel2 * m2Dif * invMassSum);
+                        Vector2 newVel1 = (vel1 * m1Dif * invMassSum) + (vel2 * 2 * Circles[c2].GetMass() * invMassSum);
+                        Vector2 newVel2 = (vel1 * 2 * Circles[c1].GetMass() * invMassSum) + (vel2 * m2Dif * invMassSum);
 
-                    Circles[c1].GetComponent<Rigidbody2D>().velocity = newVel1;
-                    Circles[c2].GetComponent<Rigidbody2D>().velocity = newVel2;
+                        Circles[c1].GetComponent<Rigidbody2D>().velocity = newVel1;
+                        Circles[c2].GetComponent<Rigidbody2D>().velocity = newVel2;
 
-                    //make sure they are not intersecting
-                    Vector3 centerDir = Circles[c2].transform.position - Circles[c1].transform.position;
-                    float overLapMag = (Circles[c1].GetRadius() + Circles[c2].GetRadius()) - centerDir.magnitude;
-                    centerDir.Normalize();
+                        //make sure they are not intersecting
+                        Vector3 centerDir = Circles[c2].transform.position - Circles[c1].transform.position;
+                        float overLapMag = (Circles[c1].GetRadius() + Circles[c2].GetRadius()) - centerDir.magnitude;
+                        centerDir.Normalize();
 
-                    Circles[c1].transform.position = Circles[c1].transform.position - centerDir * overLapMag / 2f;
-                    Circles[c2].transform.position = Circles[c2].transform.position + centerDir * overLapMag / 2f;
-
-                    
+                        Circles[c1].transform.position = Circles[c1].transform.position - centerDir * overLapMag / 2f;
+                        Circles[c2].transform.position = Circles[c2].transform.position + centerDir * overLapMag / 2f;
+                    }  
                 }
+            }
+        }
+        else
+        {
+            foreach (Circle c in Circles)
+            {
+                Vector2 vel = c.GetComponent<Rigidbody2D>().velocity;
+                c.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                c.GetComponent<Rigidbody2D>().AddForce(vel.normalized * Speed);
             }
         }
     }
