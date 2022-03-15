68a59af4b357110ca79d8b19f9bb4a707f5bae21
mattcmerritt <mattcmerritt@gmail.com>
2022-03-15 16:47:45 -0400

    using quadtree to make octree

--- Assets/Scripts/3D/Sphere.cs
+++ Assets/Scripts/3D/Sphere.cs
@@ -14,6 +14,18 @@ namespace ThreeDimensions
             return Vector3.Distance(transform.position, sphere.transform.position) < Radius + sphere.GetRadius();
         }
 
+        public bool IsColliding(float x, float y, float z, float xDepth, float yDepth, float zDepth)
+        {
+
+            float closestX = Mathf.Clamp(transform.position.x, x - xDepth/2f, x + xDepth/2f);
+            float closestY = Mathf.Clamp(transform.position.y, y - yDepth/2f, y + yDepth/2f);
+            float closestZ = Mathf.Clamp(transform.position.z, z - zDepth/2f, z + zDepth/2f);
+
+            Vector3 closestPt = new Vector3(closestX, closestY, closestZ);
+            return Vector3.Distance(transform.position, closestPt) < Radius;
+
+        }
+
         public float GetRadius()
         {
             return Radius;
